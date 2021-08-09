68a8933f5242065c6ac6fa59a6e1c795908b0039
Hos <shamsoft2006@yahoo.com>
2021-08-09 16:37:50 -0400

    add build configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'UI',
+      fileName: (format) => `ui.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'React',
+        },
+      },
+    },
+  },
+});
